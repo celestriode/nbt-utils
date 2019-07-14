5af6686f91c2b3e20ade8c579484b011bb997f7c
Celestriode <celestriode@gmail.com>
2019-07-14 02:11:43 -0400

    ctrl+s

--- src/Nbt/ByteTag.php
+++ src/Nbt/ByteTag.php
@@ -14,7 +14,7 @@ class ByteTag extends AbstractNumberTag
     {
         if (!is_numeric($value)) {
 
-            throw new \InvalidArgumentException('Value must be numeric');
+            throw new NbtFormatException('Value must be numeric');
         }
 
         $value = (int)$value;
